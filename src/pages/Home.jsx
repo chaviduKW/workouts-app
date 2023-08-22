4782b7ddaa110a12d060d434145d010e94211bc2
Chavidu <chavidukalpana40@gmail.com>
2023-08-22 22:22:46 +0530

    adding context

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,36 @@
-import { useEffect, useState } from "react"
-import WorkoutDetails from '../components/WorkoutDetails'
-import WorkoutForm from '../components/WorkoutForm'
+import { useEffect } from "react"
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+
+// components
+import WorkoutDetails from "../components/WorkoutDetails"
+import WorkoutForm from "../components/WorkoutForm"
 
 const Home = () => {
-    const [workouts, setWorkouts] = useState(null)
-    useEffect(() => {
-        const fetchWorkouts = async () => {
-            console.log("something")
-            // const response = await fetch('/api/workouts/');
-            const response = await fetch('http://localhost:4000/api/workouts/');
-            console.log(response)
-            const json = await response.json()
+  const { workouts, dispatch } = useWorkoutsContext()
 
-            if (response.ok) {
-                console.log(json)
-                setWorkouts(json)
-            }
-        }
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch('http://localhost:4000/api/workouts/')
+      const json = await response.json()
 
-        fetchWorkouts()
-    }, [])
+      if (response.ok) {
+        dispatch({type: 'SET_WORKOUTS', payload: json})
+      }
+    }
 
-    return (
-        <div className="home">
-            <div className="workouts">
-                {workouts && workouts.map((workout) => (
-                    <WorkoutDetails key={workout._id} workout={workout} />
-                ))}
-            </div>
-            <WorkoutForm/>
-        </div>
-    )
+    fetchWorkouts()
+  }, [dispatch])
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts && workouts.map(workout => (
+          <WorkoutDetails workout={workout} key={workout._id} />
+        ))}
+      </div>
+      <WorkoutForm />
+    </div>
+  )
 }
 
 export default Home
